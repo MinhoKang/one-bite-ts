6708022c98f14a3357b69025441501e75cda934f
Minho Kang <rkdalsgh0106@naver.com>
2024-03-14 15:53:38 +0900

    add todo useReducer version

--- section11/src/App.tsx
+++ section11/src/App.tsx
@@ -1,11 +1,30 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useReducer, useRef, useState } from "react";
 import "./App.css";
 import Editor from "./components/Editor";
 import { Todo } from "./type";
 import TodoItem from "./components/TodoItem";
 
+type Action =
+  | {
+      type: "CREATE";
+      data: {
+        id: number;
+        content: string;
+      };
+    }
+  | { type: "DELETE"; id: number };
+
+function reducer(state: Todo[], action: Action) {
+  switch (action.type) {
+    case "CREATE":
+      return [...state, action.data];
+    case "DELETE":
+      return state.filter((todo) => todo.id !== action.id);
+  }
+}
+
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, dispatch] = useReducer(reducer, []);
   // useState는 초기값으로 타입을 추론
   // useState()라면 undefined로 추론
   // useState<string>()라면 string | undefined로 추론 -> 초기값이 없기 때문에
@@ -14,11 +33,17 @@ function App() {
   const idRef = useRef(0);
 
   const onClickAdd = (text: string) => {
-    setTodos([...todos, { id: idRef.current++, content: text }]);
+    dispatch({
+      type: "CREATE",
+      data: { id: idRef.current++, content: text },
+    });
   };
 
   const onClickDelete = (id: number) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
+    dispatch({
+      type: "DELETE",
+      id,
+    });
   };
 
   useEffect(() => {
